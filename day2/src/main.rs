8fce4bc0794ba20f66a06ab429eff89330c3896d
Stef <stef.robbe@gmail.com>
2021-12-02 17:45:33 +0100

    day 2, part 2 done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -17,7 +17,7 @@ enum Direction {
 #[derive(Debug)]
 struct Movement {
     direction: Direction,
-    amount: u32
+    amount: i32
 }
 
 impl FromStr for Movement {
@@ -27,30 +27,37 @@ impl FromStr for Movement {
         let movement: Vec<&str> = s.split(' ').collect();
 
         let direction_fromstr = movement[0].parse::<Direction>().unwrap();
-        let amount_fromstr = movement[1].parse::<u32>()?;
+        let amount_fromstr = movement[1].parse::<i32>()?;
 
         Ok(Movement { direction: direction_fromstr, amount: amount_fromstr })
     }
 }
 
+// https://cotigao.medium.com/mutable-reference-in-rust-995320366e22
+fn move_forward(horizontal: &mut i32, depth: &mut i32, aim: &i32, amount: &i32) {
+    *horizontal += amount;
+    *depth += aim * amount;
+}
+
 fn main() {
     let file = fs::read_to_string("input.txt").unwrap();
 
-    let mut horizontal = 0u32;
-    let mut depth = 0u32;
+    let mut horizontal = 0i32;
+    let mut depth = 0i32;
+    let mut aim = 0i32;
 
     for line in file.split("\n") {
         if line == "" { continue; }
         let movement = line.parse::<Movement>().unwrap();
 
         match movement.direction {
-            Direction::Forward => horizontal += movement.amount,
-            Direction::Down => depth += movement.amount,
-            Direction::Up => depth -= movement.amount
+            Direction::Forward => move_forward(&mut horizontal, &mut depth, &aim, &movement.amount),
+            Direction::Down => aim += movement.amount as i32,
+            Direction::Up => aim -= movement.amount as i32
         };
 
     }
 
     println!("{} * {} = {}", horizontal, depth, horizontal * depth)
-    // 1693300
+    // 1857958050
 }
