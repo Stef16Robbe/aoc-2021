4d2f502efbda54ac50b0bf05735cc1940e1e3a70
Stef <stef.robbe@gmail.com>
2021-12-07 20:16:55 +0100

    day 7, part 2 done

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,22 +1,25 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+fn calculate_steps(n: u64, mut total: u64) -> u64 {
+    total += n;
+    if n == 0 { return total }
+    calculate_steps(n - 1, total)
+}
+
 fn calculate_fuel_costs(crabs: &Vec<i32>, position: &i32) -> u64 {
     let mut fuel_cost: u64 = 0;
 
     for crab in crabs {
         // https://doc.rust-lang.org/core/?search=abs
-        fuel_cost += (crab - position).abs() as u64;
+        fuel_cost += calculate_steps((crab - position).abs() as u64, 0);
     }
 
     fuel_cost
 }
 
-/*
-"Dumb" method:
-- get min and max from nums
-- for min..max: calculate fuel cost for each option
-*/
+// One might say this is not the most 
+// efficient method...
 fn main() {
     let mut file = BufReader::new(File::open("input.txt").unwrap());
     let mut numbers = String::new();
@@ -41,4 +44,5 @@ fn main() {
     }
 
     println!("{:?}", costs.iter().min().unwrap());
+    // 95476244
 }
